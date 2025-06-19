742f1faa0e0fe821bef2bba95cb010ca8d6380bc
Spoc Web <10584012+SpocWeb@users.noreply.github.com>
2025-06-19 14:48:37 +0200

    auto-commit

---
+++ Germany~West/checkout-Baden-Wuerttemberg.bat
@@ -0,0 +1,1 @@
+git clone "https://github.com/SpocWiki/Germany-Baden-Wuerttemberg" Baden-Wuerttemberg
---
+++ Germany~West/checkout-Rheinland-Pfalz.bat
@@ -0,0 +1,1 @@
+git clone "https://github.com/SpocWiki/Germany-Rheinland-Pfalz" Rheinland-Pfalz
